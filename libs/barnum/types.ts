773ac5f0f61c9596cfc4466dbd64fe92e1d05eb1
Robert Balicki <robert.balicki@gmail.com>
2026-03-24 18:37:51 -0400

    Fix HandlerDefinition: relax ZodType input constraint to support .default()

--- libs/barnum/types.ts
+++ libs/barnum/types.ts
@@ -1,8 +1,9 @@
 import type { z } from "zod";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export interface HandlerDefinition<C = unknown, V = unknown> {
-  stepConfigValidator: z.ZodType<C>;
-  getStepValueValidator: (stepConfig: C) => z.ZodType<V>;
+  stepConfigValidator: z.ZodType<C, z.ZodTypeDef, any>;
+  getStepValueValidator: (stepConfig: C) => z.ZodType<V, z.ZodTypeDef, any>;
   handle: (context: HandlerContext<C, V>) => Promise<FollowUpTask[]>;
 }
 
