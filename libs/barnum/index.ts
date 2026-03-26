68fd94ca47e802e1e9811af0cd898dd393010777
Robert Balicki <robert.balicki@gmail.com>
2026-03-26 13:41:39 -0400

    Add Handler class, createHandler, and isHandler to libs/barnum

--- libs/barnum/index.ts
+++ libs/barnum/index.ts
@@ -1,6 +1,7 @@
 export * from "./barnum-config-schema.zod.js";
 export * from "./barnum-cli-schema.zod.js";
 export { BarnumConfig, type RunOptions } from "./run.js";
+export { Handler, createHandler, isHandler } from "./types.js";
 export type {
   HandlerDefinition,
   HandlerContext,
