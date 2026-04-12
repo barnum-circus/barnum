888f21ebf706a3d2fc8a5f99e038d96892d413db
Robert Balicki <robert.balicki@gmail.com>
2026-04-11 21:28:55 -0400

    Tier 3.6: Rename bare type params to descriptive names
    all.ts: In→TInput, O1-O10→TOut1-TOut10
    pipe.ts: T1-T11→TStep1-TStep11

--- libs/barnum/src/all.ts
+++ libs/barnum/src/all.ts
@@ -8,79 +8,123 @@ import { constant } from "./builtins.js";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function all(): TypedAction<any, []>;
-export function all<In, O1>(a1: Pipeable<In, O1>): TypedAction<In, [O1]>;
-export function all<In, O1, O2>(
-  a1: Pipeable<In, O1>,
-  a2: Pipeable<In, O2>,
-): TypedAction<In, [O1, O2]>;
-export function all<In, O1, O2, O3>(
-  a1: Pipeable<In, O1>,
-  a2: Pipeable<In, O2>,
-  a3: Pipeable<In, O3>,
-): TypedAction<In, [O1, O2, O3]>;
-export function all<In, O1, O2, O3, O4>(
-  a1: Pipeable<In, O1>,
-  a2: Pipeable<In, O2>,
-  a3: Pipeable<In, O3>,
-  a4: Pipeable<In, O4>,
-): TypedAction<In, [O1, O2, O3, O4]>;
-export function all<In, O1, O2, O3, O4, O5>(
-  a1: Pipeable<In, O1>,
-  a2: Pipeable<In, O2>,
-  a3: Pipeable<In, O3>,
-  a4: Pipeable<In, O4>,
-  a5: Pipeable<In, O5>,
-): TypedAction<In, [O1, O2, O3, O4, O5]>;
-export function all<In, O1, O2, O3, O4, O5, O6>(
-  a1: Pipeable<In, O1>,
-  a2: Pipeable<In, O2>,
-  a3: Pipeable<In, O3>,
-  a4: Pipeable<In, O4>,
-  a5: Pipeable<In, O5>,
-  a6: Pipeable<In, O6>,
-): TypedAction<In, [O1, O2, O3, O4, O5, O6]>;
-export function all<In, O1, O2, O3, O4, O5, O6, O7>(
-  a1: Pipeable<In, O1>,
-  a2: Pipeable<In, O2>,
-  a3: Pipeable<In, O3>,
-  a4: Pipeable<In, O4>,
-  a5: Pipeable<In, O5>,
-  a6: Pipeable<In, O6>,
-  a7: Pipeable<In, O7>,
-): TypedAction<In, [O1, O2, O3, O4, O5, O6, O7]>;
-export function all<In, O1, O2, O3, O4, O5, O6, O7, O8>(
-  a1: Pipeable<In, O1>,
-  a2: Pipeable<In, O2>,
-  a3: Pipeable<In, O3>,
-  a4: Pipeable<In, O4>,
-  a5: Pipeable<In, O5>,
-  a6: Pipeable<In, O6>,
-  a7: Pipeable<In, O7>,
-  a8: Pipeable<In, O8>,
-): TypedAction<In, [O1, O2, O3, O4, O5, O6, O7, O8]>;
-export function all<In, O1, O2, O3, O4, O5, O6, O7, O8, O9>(
-  a1: Pipeable<In, O1>,
-  a2: Pipeable<In, O2>,
-  a3: Pipeable<In, O3>,
-  a4: Pipeable<In, O4>,
-  a5: Pipeable<In, O5>,
-  a6: Pipeable<In, O6>,
-  a7: Pipeable<In, O7>,
-  a8: Pipeable<In, O8>,
-  a9: Pipeable<In, O9>,
-): TypedAction<In, [O1, O2, O3, O4, O5, O6, O7, O8, O9]>;
-export function all<In, O1, O2, O3, O4, O5, O6, O7, O8, O9, O10>(
-  a1: Pipeable<In, O1>,
-  a2: Pipeable<In, O2>,
-  a3: Pipeable<In, O3>,
-  a4: Pipeable<In, O4>,
-  a5: Pipeable<In, O5>,
-  a6: Pipeable<In, O6>,
-  a7: Pipeable<In, O7>,
-  a8: Pipeable<In, O8>,
-  a9: Pipeable<In, O9>,
-  a10: Pipeable<In, O10>,
-): TypedAction<In, [O1, O2, O3, O4, O5, O6, O7, O8, O9, O10]>;
+export function all<TInput, TOut1>(
+  a1: Pipeable<TInput, TOut1>,
+): TypedAction<TInput, [TOut1]>;
+export function all<TInput, TOut1, TOut2>(
+  a1: Pipeable<TInput, TOut1>,
+  a2: Pipeable<TInput, TOut2>,
+): TypedAction<TInput, [TOut1, TOut2]>;
+export function all<TInput, TOut1, TOut2, TOut3>(
+  a1: Pipeable<TInput, TOut1>,
+  a2: Pipeable<TInput, TOut2>,
+  a3: Pipeable<TInput, TOut3>,
+): TypedAction<TInput, [TOut1, TOut2, TOut3]>;
+export function all<TInput, TOut1, TOut2, TOut3, TOut4>(
+  a1: Pipeable<TInput, TOut1>,
+  a2: Pipeable<TInput, TOut2>,
+  a3: Pipeable<TInput, TOut3>,
+  a4: Pipeable<TInput, TOut4>,
+): TypedAction<TInput, [TOut1, TOut2, TOut3, TOut4]>;
+export function all<TInput, TOut1, TOut2, TOut3, TOut4, TOut5>(
+  a1: Pipeable<TInput, TOut1>,
+  a2: Pipeable<TInput, TOut2>,
+  a3: Pipeable<TInput, TOut3>,
+  a4: Pipeable<TInput, TOut4>,
+  a5: Pipeable<TInput, TOut5>,
+): TypedAction<TInput, [TOut1, TOut2, TOut3, TOut4, TOut5]>;
+export function all<TInput, TOut1, TOut2, TOut3, TOut4, TOut5, TOut6>(
+  a1: Pipeable<TInput, TOut1>,
+  a2: Pipeable<TInput, TOut2>,
+  a3: Pipeable<TInput, TOut3>,
+  a4: Pipeable<TInput, TOut4>,
+  a5: Pipeable<TInput, TOut5>,
+  a6: Pipeable<TInput, TOut6>,
+): TypedAction<TInput, [TOut1, TOut2, TOut3, TOut4, TOut5, TOut6]>;
+export function all<TInput, TOut1, TOut2, TOut3, TOut4, TOut5, TOut6, TOut7>(
+  a1: Pipeable<TInput, TOut1>,
+  a2: Pipeable<TInput, TOut2>,
+  a3: Pipeable<TInput, TOut3>,
+  a4: Pipeable<TInput, TOut4>,
+  a5: Pipeable<TInput, TOut5>,
+  a6: Pipeable<TInput, TOut6>,
+  a7: Pipeable<TInput, TOut7>,
+): TypedAction<TInput, [TOut1, TOut2, TOut3, TOut4, TOut5, TOut6, TOut7]>;
+export function all<
+  TInput,
+  TOut1,
+  TOut2,
+  TOut3,
+  TOut4,
+  TOut5,
+  TOut6,
+  TOut7,
+  TOut8,
+>(
+  a1: Pipeable<TInput, TOut1>,
+  a2: Pipeable<TInput, TOut2>,
+  a3: Pipeable<TInput, TOut3>,
+  a4: Pipeable<TInput, TOut4>,
+  a5: Pipeable<TInput, TOut5>,
+  a6: Pipeable<TInput, TOut6>,
+  a7: Pipeable<TInput, TOut7>,
+  a8: Pipeable<TInput, TOut8>,
+): TypedAction<
+  TInput,
+  [TOut1, TOut2, TOut3, TOut4, TOut5, TOut6, TOut7, TOut8]
+>;
+export function all<
+  TInput,
+  TOut1,
+  TOut2,
+  TOut3,
+  TOut4,
+  TOut5,
+  TOut6,
+  TOut7,
+  TOut8,
+  TOut9,
+>(
+  a1: Pipeable<TInput, TOut1>,
+  a2: Pipeable<TInput, TOut2>,
+  a3: Pipeable<TInput, TOut3>,
+  a4: Pipeable<TInput, TOut4>,
+  a5: Pipeable<TInput, TOut5>,
+  a6: Pipeable<TInput, TOut6>,
+  a7: Pipeable<TInput, TOut7>,
+  a8: Pipeable<TInput, TOut8>,
+  a9: Pipeable<TInput, TOut9>,
+): TypedAction<
+  TInput,
+  [TOut1, TOut2, TOut3, TOut4, TOut5, TOut6, TOut7, TOut8, TOut9]
+>;
+export function all<
+  TInput,
+  TOut1,
+  TOut2,
+  TOut3,
+  TOut4,
+  TOut5,
+  TOut6,
+  TOut7,
+  TOut8,
+  TOut9,
+  TOut10,
+>(
+  a1: Pipeable<TInput, TOut1>,
+  a2: Pipeable<TInput, TOut2>,
+  a3: Pipeable<TInput, TOut3>,
+  a4: Pipeable<TInput, TOut4>,
+  a5: Pipeable<TInput, TOut5>,
+  a6: Pipeable<TInput, TOut6>,
+  a7: Pipeable<TInput, TOut7>,
+  a8: Pipeable<TInput, TOut8>,
+  a9: Pipeable<TInput, TOut9>,
+  a10: Pipeable<TInput, TOut10>,
+): TypedAction<
+  TInput,
+  [TOut1, TOut2, TOut3, TOut4, TOut5, TOut6, TOut7, TOut8, TOut9, TOut10]
+>;
 export function all(...actions: Action[]): Action {
   if (actions.length === 0) {
     return constant([]);
--- libs/barnum/src/pipe.ts
+++ libs/barnum/src/pipe.ts
@@ -2,79 +2,123 @@ import type { Action, PipeIn, Pipeable, TypedAction } from "./ast.js";
 import { chain } from "./chain.js";
 import { identity } from "./builtins.js";
 
-export function pipe<T1, T2>(a1: Pipeable<T1, T2>): TypedAction<PipeIn<T1>, T2>;
-export function pipe<T1, T2, T3>(
-  a1: Pipeable<T1, T2>,
-  a2: Pipeable<T2, T3>,
-): TypedAction<PipeIn<T1>, T3>;
-export function pipe<T1, T2, T3, T4>(
-  a1: Pipeable<T1, T2>,
-  a2: Pipeable<T2, T3>,
-  a3: Pipeable<T3, T4>,
-): TypedAction<PipeIn<T1>, T4>;
-export function pipe<T1, T2, T3, T4, T5>(
-  a1: Pipeable<T1, T2>,
-  a2: Pipeable<T2, T3>,
-  a3: Pipeable<T3, T4>,
-  a4: Pipeable<T4, T5>,
-): TypedAction<PipeIn<T1>, T5>;
-export function pipe<T1, T2, T3, T4, T5, T6>(
-  a1: Pipeable<T1, T2>,
-  a2: Pipeable<T2, T3>,
-  a3: Pipeable<T3, T4>,
-  a4: Pipeable<T4, T5>,
-  a5: Pipeable<T5, T6>,
-): TypedAction<PipeIn<T1>, T6>;
-export function pipe<T1, T2, T3, T4, T5, T6, T7>(
-  a1: Pipeable<T1, T2>,
-  a2: Pipeable<T2, T3>,
-  a3: Pipeable<T3, T4>,
-  a4: Pipeable<T4, T5>,
-  a5: Pipeable<T5, T6>,
-  a6: Pipeable<T6, T7>,
-): TypedAction<PipeIn<T1>, T7>;
-export function pipe<T1, T2, T3, T4, T5, T6, T7, T8>(
-  a1: Pipeable<T1, T2>,
-  a2: Pipeable<T2, T3>,
-  a3: Pipeable<T3, T4>,
-  a4: Pipeable<T4, T5>,
-  a5: Pipeable<T5, T6>,
-  a6: Pipeable<T6, T7>,
-  a7: Pipeable<T7, T8>,
-): TypedAction<PipeIn<T1>, T8>;
-export function pipe<T1, T2, T3, T4, T5, T6, T7, T8, T9>(
-  a1: Pipeable<T1, T2>,
-  a2: Pipeable<T2, T3>,
-  a3: Pipeable<T3, T4>,
-  a4: Pipeable<T4, T5>,
-  a5: Pipeable<T5, T6>,
-  a6: Pipeable<T6, T7>,
-  a7: Pipeable<T7, T8>,
-  a8: Pipeable<T8, T9>,
-): TypedAction<PipeIn<T1>, T9>;
-export function pipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
-  a1: Pipeable<T1, T2>,
-  a2: Pipeable<T2, T3>,
-  a3: Pipeable<T3, T4>,
-  a4: Pipeable<T4, T5>,
-  a5: Pipeable<T5, T6>,
-  a6: Pipeable<T6, T7>,
-  a7: Pipeable<T7, T8>,
-  a8: Pipeable<T8, T9>,
-  a9: Pipeable<T9, T10>,
-): TypedAction<PipeIn<T1>, T10>;
-export function pipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(
-  a1: Pipeable<T1, T2>,
-  a2: Pipeable<T2, T3>,
-  a3: Pipeable<T3, T4>,
-  a4: Pipeable<T4, T5>,
-  a5: Pipeable<T5, T6>,
-  a6: Pipeable<T6, T7>,
-  a7: Pipeable<T7, T8>,
-  a8: Pipeable<T8, T9>,
-  a9: Pipeable<T9, T10>,
-  a10: Pipeable<T10, T11>,
-): TypedAction<PipeIn<T1>, T11>;
+export function pipe<TStep1, TStep2>(
+  a1: Pipeable<TStep1, TStep2>,
+): TypedAction<PipeIn<TStep1>, TStep2>;
+export function pipe<TStep1, TStep2, TStep3>(
+  a1: Pipeable<TStep1, TStep2>,
+  a2: Pipeable<TStep2, TStep3>,
+): TypedAction<PipeIn<TStep1>, TStep3>;
+export function pipe<TStep1, TStep2, TStep3, TStep4>(
+  a1: Pipeable<TStep1, TStep2>,
+  a2: Pipeable<TStep2, TStep3>,
+  a3: Pipeable<TStep3, TStep4>,
+): TypedAction<PipeIn<TStep1>, TStep4>;
+export function pipe<TStep1, TStep2, TStep3, TStep4, TStep5>(
+  a1: Pipeable<TStep1, TStep2>,
+  a2: Pipeable<TStep2, TStep3>,
+  a3: Pipeable<TStep3, TStep4>,
+  a4: Pipeable<TStep4, TStep5>,
+): TypedAction<PipeIn<TStep1>, TStep5>;
+export function pipe<TStep1, TStep2, TStep3, TStep4, TStep5, TStep6>(
+  a1: Pipeable<TStep1, TStep2>,
+  a2: Pipeable<TStep2, TStep3>,
+  a3: Pipeable<TStep3, TStep4>,
+  a4: Pipeable<TStep4, TStep5>,
+  a5: Pipeable<TStep5, TStep6>,
+): TypedAction<PipeIn<TStep1>, TStep6>;
+export function pipe<TStep1, TStep2, TStep3, TStep4, TStep5, TStep6, TStep7>(
+  a1: Pipeable<TStep1, TStep2>,
+  a2: Pipeable<TStep2, TStep3>,
+  a3: Pipeable<TStep3, TStep4>,
+  a4: Pipeable<TStep4, TStep5>,
+  a5: Pipeable<TStep5, TStep6>,
+  a6: Pipeable<TStep6, TStep7>,
+): TypedAction<PipeIn<TStep1>, TStep7>;
+export function pipe<
+  TStep1,
+  TStep2,
+  TStep3,
+  TStep4,
+  TStep5,
+  TStep6,
+  TStep7,
+  TStep8,
+>(
+  a1: Pipeable<TStep1, TStep2>,
+  a2: Pipeable<TStep2, TStep3>,
+  a3: Pipeable<TStep3, TStep4>,
+  a4: Pipeable<TStep4, TStep5>,
+  a5: Pipeable<TStep5, TStep6>,
+  a6: Pipeable<TStep6, TStep7>,
+  a7: Pipeable<TStep7, TStep8>,
+): TypedAction<PipeIn<TStep1>, TStep8>;
+export function pipe<
+  TStep1,
+  TStep2,
+  TStep3,
+  TStep4,
+  TStep5,
+  TStep6,
+  TStep7,
+  TStep8,
+  TStep9,
+>(
+  a1: Pipeable<TStep1, TStep2>,
+  a2: Pipeable<TStep2, TStep3>,
+  a3: Pipeable<TStep3, TStep4>,
+  a4: Pipeable<TStep4, TStep5>,
+  a5: Pipeable<TStep5, TStep6>,
+  a6: Pipeable<TStep6, TStep7>,
+  a7: Pipeable<TStep7, TStep8>,
+  a8: Pipeable<TStep8, TStep9>,
+): TypedAction<PipeIn<TStep1>, TStep9>;
+export function pipe<
+  TStep1,
+  TStep2,
+  TStep3,
+  TStep4,
+  TStep5,
+  TStep6,
+  TStep7,
+  TStep8,
+  TStep9,
+  TStep10,
+>(
+  a1: Pipeable<TStep1, TStep2>,
+  a2: Pipeable<TStep2, TStep3>,
+  a3: Pipeable<TStep3, TStep4>,
+  a4: Pipeable<TStep4, TStep5>,
+  a5: Pipeable<TStep5, TStep6>,
+  a6: Pipeable<TStep6, TStep7>,
+  a7: Pipeable<TStep7, TStep8>,
+  a8: Pipeable<TStep8, TStep9>,
+  a9: Pipeable<TStep9, TStep10>,
+): TypedAction<PipeIn<TStep1>, TStep10>;
+export function pipe<
+  TStep1,
+  TStep2,
+  TStep3,
+  TStep4,
+  TStep5,
+  TStep6,
+  TStep7,
+  TStep8,
+  TStep9,
+  TStep10,
+  TStep11,
+>(
+  a1: Pipeable<TStep1, TStep2>,
+  a2: Pipeable<TStep2, TStep3>,
+  a3: Pipeable<TStep3, TStep4>,
+  a4: Pipeable<TStep4, TStep5>,
+  a5: Pipeable<TStep5, TStep6>,
+  a6: Pipeable<TStep6, TStep7>,
+  a7: Pipeable<TStep7, TStep8>,
+  a8: Pipeable<TStep8, TStep9>,
+  a9: Pipeable<TStep9, TStep10>,
+  a10: Pipeable<TStep10, TStep11>,
+): TypedAction<PipeIn<TStep1>, TStep11>;
 export function pipe(...actions: Action[]): Action {
   if (actions.length === 0) {
     return identity();
